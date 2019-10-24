570542f7c82d7fa1d934e7cb903d77dbc3cf00f0
Alex Sivkov <202493@mail.ru>
2019-10-24 15:45:21 +0400

    add buttons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+const MobileService = [
+  { name: "МТС", zip: "0" },
+  { name: "Билайн", zip: "1" },
+  { name: "Мегафон", zip: "2" },
+  { name: "ТEL2", zip: "3" }
+   ];
+
+
+ class ButtonsDisplay extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    activePlace: 0
+    };
+  }
+  render() {
+    const activePlace = this.state.activePlace;
+
+    return (
+      <div className="App">
+           {MobileService.map((place, index) => (
+           <button className="button13"
+            key={index}
+            onClick={() => {
+              this.setState({ activePlace: index });
+              alert (index)
+            }}
+          >
+            <img src={`/images/${index}.png`} className="App-logo" alt="logo" />
+
+           
+           
+            {place.name}
+          </button>
+        ))}
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Оплати мобильную связь</h1>
+       
+        < ButtonsDisplay />
         <a
           className="App-link"
           href="https://reactjs.org"
