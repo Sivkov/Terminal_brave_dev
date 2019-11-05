0cd76a03554b1e43677693af1079d30288de0951
Alex Sivkov <202493@mail.ru>
2019-11-05 19:10:49 +0400

    final

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { UserForm } from './UserForm';
 import { ButtonsDisplay } from './ButtonsDisplay';
 import { WaitingServer } from './WaitingServer';
 
-
 const Header = styled.header`
     background-color: #282c34;
     min-height: 100vh;
@@ -15,46 +14,44 @@ const Header = styled.header`
     font-size: calc(10px + 2vmin);
     color: white;
 `
-
 const AppWrapper = styled.div`
     text-align: center;
 `
-
-
 class App extends React.Component {
-  constructor() {
-      super()
-      this.state = { payActive: 1, operator: '', mobile: '', phone : '', sum : ''  }
-      this.nextPanel = this.nextPanel.bind(this)  
-  };
+    constructor() {
+        super()
+        this.state = { payActive: 1, operator: '', mobile: '', phone : '', sum : ''  }
+        this.nextPanel = this.nextPanel.bind(this)  
+    };
 
-  nextPanel (step, mobile, phone, sum) {
-      this.setState(prevState => {
+    nextPanel (step, mobile, phone, sum) {
+        this.setState(prevState => {
           return {
           payActive: prevState.payActive + step
           }
-      })
-      this.setState({operator : mobile, phone: phone, sum: sum }) 
-  };
+        })
+        this.setState({operator : mobile, phone: phone, sum: sum }) 
+    };
   
-  render() {
-      return (
-        <AppWrapper >
-            <Header>
-                {/*выбор оператора*/}
-                {this.state.payActive== 1 ? < ButtonsDisplay steps={this.nextPanel} />  :  '' }
+    render() {
+        return (
+            <AppWrapper >
+                <Header>
+                    {/*выбор оператора*/}
+                    {this.state.payActive== 1 ? < ButtonsDisplay steps={this.nextPanel} />  :  '' }
 
-                {/*ввод данных в форму*/}
-                {this.state.payActive== 2 ? < UserForm  steps={this.nextPanel} operator={this.state.operator} />  : '' }
+                    {/*ввод данных в форму или на првый экран для смены оператора*/}
+                    {this.state.payActive== 2 ? < UserForm  steps={this.nextPanel} operator={this.state.operator} />  : '' }
 
-                {/* ожидание ответа и возврат в шаг первый */}
-                {this.state.payActive==  3 ? < WaitingServer 
-                  steps={this.nextPanel}  operator={this.state.operator}
-                  phone={this.state.phone} sum={this.state.sum} />  : '' }
-            </Header>
-        </AppWrapper>
-      )
-  }
+                    {/* ожидание ответа и возврат в шаг первый */}
+                    {this.state.payActive==  3 ? < WaitingServer 
+                    steps={this.nextPanel}  operator={this.state.operator}
+                    phone={this.state.phone} sum={this.state.sum} />  : '' }
+                    
+                </Header>
+            </AppWrapper>
+       )
+    }
 }
 
 export default App;
